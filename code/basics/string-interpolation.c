8499ce6e4ff5bd64a60709d763c1fe2481f9fcc8
Klaus Wenger <kjwenger@yahoo.com>
2018-01-28 21:19:03 +0100

    fixed C string interpolation

--- code/basics/string-interpolation.c
+++ code/basics/string-interpolation.c
@@ -2,4 +2,7 @@
 const int apples = 3;
 const int oranges = 5;
 char[1024] fruitSummary;
-sprintf("I have %d %s", apples + oranges, "pieces of fruit.");
+sprintf(fruitSummary,
+  "I have %d %s",
+  apples + oranges,
+  "pieces of fruit.");
