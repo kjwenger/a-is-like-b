d3d016f7c93e40b31da4e8fa42e5c5a9fc8725ac
Klaus Wenger <kjwenger@yahoo.com>
2018-03-12 19:13:48 +0100

    Adding Groovy: Hello, world!

--- code/basics/hello-world.groovy
+++ code/basics/hello-world.groovy
@@ -1,6 +1,6 @@
 class HelloWorld {
-    static void main(
-            String[] args) {
-        println("Hello, world!")
-    }
+  static void main(
+      String[] args) {
+    println("Hello, world!")
+  }
 }
